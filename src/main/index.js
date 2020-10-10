5c6479b09afc187ecd2c53dea6e7f82430f56117
will-dev <w1llmarcio.dev@hotmail.com>
2020-10-09 22:46:54 -0300

    add counterProvider

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,19 @@
 import React from "react";
+import "./style.css";
+// components
+import First from "../components/First";
+import Second from "../components/Second";
+import Third from "../components/Third";
 
-export const App = () => <div>hello World</div>;
+// Context
+import { CounterProvider } from "../context/Counter";
+
+export const App = () => (
+  <CounterProvider>
+    <div className="wrapper">
+      <First />
+      <Second />
+      <Third />
+    </div>
+  </CounterProvider>
+);
